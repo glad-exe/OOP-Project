fb47e6951f67484fe74ad959b2aad82775ad0f36
Ahmad Dweikat <115323763+AhmadArchaic@users.noreply.github.com>
2026-08-02 18:41:51 +0300

    Header file ez,fully ready

--- Inventory.h
+++ Inventory.h
@@ -1,5 +1,37 @@
 // Group Names: 1. Ahmad Enad Dweikat 2. Mohammad Araby Al-kabbani 3. Munther Walid Alhawamdeh 4. Layth Marwan Allouzi
 // Student ID: 20251030 20250141 20250032 20230270
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
+#include "Product.h"
 
-//PLACEHOLDER FILE FOR INVENTORY CLASS. NOT USED YET. PLS FIX
+class Inventory{
+    private:
+        Product*  shelf[50];      // shelf is always 50, never changes
+        int       shelfCount;     // how many are used rn
+
+        Product** warehouse;      // size given when program starts
+        int       warehouseSize;  // max size of warehosue
+        int       warehouseCount; // how many are used
+
+        bool sameText(const char* a, const char* b) const; //checks if 2 strings are equal
+
+    public:
+        //CONSTRUCTOR
+        Inventory(int size);
+
+        //DESTRUCTOR
+        ~Inventory();
+
+        //member funcions
+        void      addProduct(Product* product, bool toShelf = true);
+        void      removeByBarcode(int barcode);
+        Product*  findByBarcode(int barcode) const;
+        Product*  findByName(const char* name) const;
+        void      listShelf() const;
+        double    sellableStockValue() const;
+
+        friend class DailyReport;
+};
+
+#endif
