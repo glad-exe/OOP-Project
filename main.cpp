edb774d0308a23e10c5c40b657bc978430b651bf
Glados.exe <mohammadarabyalkabany@gmail.com>
2026-08-03 13:20:57 +0300

    added main.cpp... how.

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "Store.h"
+#include "Inventory.h"
+#include "Product.h"
+#include "BoxedProduct.h"
+#include "PerishableProduct.h"
+#include "DigitalProduct.h"
+#include "Order.h"
+#include "Supplier.h"
+#include "Label.h"
+#include "ShelfLocation.h"
+#include "DailyReport.h"
+#include "StringUtils.h"
+
+using namespace std;
+
+void handleAddToShelf(Store& store) {
+
+}
+
+void handleAddToWarehouse(Store& store) {
+
+}
+
+void handleDisplayShelf(Store& store) {
+
+}
+
+void handleSearchByBarcode(Store& store) {
+
+}
+
+void handleSearchByName(Store& store) {
+
+}
+
+void handleRemoveByBarcode(Store& store) {
+
+}
+
+void handleAddSupplier(Store& store) {
+
+}
+void handleStartOrder(Order orders[], int& orderCount, int maxOrders) {
+
+}
+
+void handleAddItemToOrder(Order orders[], int orderCount, Inventory& inv) {
+
+}
+
+void handleMergeOrders(Order orders[], int orderCount) {
+
+}
+
+void handleCompleteOrder(Order orders[], int orderCount, Inventory& inv) {
+
+}
+
+void handlePrintSellableStock(Store& store) {
+
+}
+
+void handlePrintProductCount() {
+
+}
+
+void handlePrintLabels(Store& store) {
+
+}
+
+void handlePrintDailyReport(Inventory& inv, Order orders[], int orderCount) {
+
+}
+
+void handleCloseShop(Store& store) {
+
+}
+
+
